d34b409b2c8313a805eadcb668b817543b329f4c
mayacahana <maya.cahana24@gmail.com>
2017-11-12 18:08:52 +0200

    script-done

--- hw1_script.sh
+++ hw1_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-directory = $1
 find . -name ${HW1DIR} -type d -print0|xargs -0 rm -r --
 mkdir -m 755 ${HW1DIR} 
 cd ${HW1DIR}
-cp $1 ${HW1TF}
-chmod 755 ${HW1TF}
-cd
+cp ${1} ./${HW1TF}
+chmod 744 ${HW1TF}
+cd ..
+./hw1_sub cat dog
